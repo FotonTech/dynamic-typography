dd741c2910d4a1146e73054ee05dfce91e12e480
gabrieldemian <gabrielgcr45@gmail.com>
2021-07-26 09:51:58 -0300

    final changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import '@fontsource/poppins'
 import '../styles/globals.css'
 
-// import '../styles/breakpoints.css'
+import '../styles/breakpoints.css'
 // import '../styles/vw.css'
 // import '../styles/fluid.css'
-import '../styles/modular.css'
+// import '../styles/modular.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
